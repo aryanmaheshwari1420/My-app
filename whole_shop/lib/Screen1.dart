26419c74e68cf816dc740871fc9441f7ce0d98e0
aryanmaheshwari1420 <98485902+aryanmaheshwari1420@users.noreply.github.com>
2022-07-03 19:14:23 +0530

    Screen1 updated

--- whole_shop/lib/Screen1.dart
+++ whole_shop/lib/Screen1.dart
@@ -1,42 +1,58 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
-
-import 'package:flutter/material.dart';
-import 'package:whole_shop/Screen2.dart';
-import 'package:whole_shop/screen4.dart';
 import 'package:whole_shop/screen3.dart';
 
+import 'Screen2.dart';
+
 class Screen1 extends StatelessWidget {
-  String title = "";
-  Screen1({Key? key, required this.title}) : super(key: key);
+  String title = '';
+  Screen1({Key? key,required this.title}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-          image: DecorationImage(
-              image: NetworkImage(
-                  "https://images.unsplash.com/photo-1557683316-973673baf926?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDE4fHx8ZW58MHx8fHw%3D&w=1000&q=80"),
-              fit: BoxFit.cover)),
-      child: Scaffold(
-        backgroundColor: Colors.blueGrey[600],
-        appBar: null,
-        body: Container(
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
             decoration: BoxDecoration(
-              image: DecorationImage(
-                  image: NetworkImage(
-                      'https://images.unsplash.com/photo-1557683316-973673baf926?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDE4fHx8ZW58MHx8fHw%3D&w=1000&q=80'),
-                  fit: BoxFit.cover),
+                image: DecorationImage(
+                    image: NetworkImage(
+                        "https://images.unsplash.com/photo-1557683316-973673baf926?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDE4fHx8ZW58MHx8fHw%3D&w=1000&q=80"),
+                    fit: BoxFit.cover)),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              // color: Colors.red,
+
+              height: 800,
+              width: 450,
+              child: Padding(
+                padding: const EdgeInsets.only(top: 95, left: 15),
+                child: Text(
+                  "Welcome to     \n     MeetMe app",
+                  style: TextStyle(
+                      fontSize: 35, color: Color.fromARGB(255, 184, 203, 204)),
+                ),
+              ),
             ),
-            child: Center(
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 370, left: 15),
+            child: Container(
+              height: 250,
+              width: 350,
+              // color: Colors.green,
               child: Column(
                 children: [
                   Container(
-                    padding: EdgeInsets.only(top: 505),
+                    padding: EdgeInsets.only(top: 50),
                     child: ElevatedButton(
                         style: ElevatedButton.styleFrom(
+                          shadowColor: Colors.lightBlueAccent,
                           padding: EdgeInsets.symmetric(
                               horizontal: 100.0, vertical: 8.0),
                           shape: StadiumBorder(),
@@ -48,8 +64,8 @@ class Screen1 extends StatelessWidget {
                           }));
                         },
                         child: Text(
-                          "Sign In",
-                          style: TextStyle(fontSize: 24),
+                          "Sign in",
+                          style: TextStyle(fontSize: 24, color: Colors.white),
                         )),
                   ),
                   const SizedBox(
@@ -58,26 +74,27 @@ class Screen1 extends StatelessWidget {
                   Container(
                     child: ElevatedButton(
                         style: ElevatedButton.styleFrom(
+                          shadowColor: Colors.lightBlueAccent,
                           padding: EdgeInsets.symmetric(
                               horizontal: 100.0, vertical: 8.0),
                           shape: StadiumBorder(),
                         ),
                         onPressed: () {
                           Navigator.push(context,
-                            MaterialPageRoute(builder: (context) {
-                          return Screen3(title: "Sign Up");
-                        }));
-
+                              MaterialPageRoute(builder: (context) {
+                            return Screen3(title: "Sign Up");
+                          }));
                         },
                         child: Text(
-                          "Sign Up",
-                          style: TextStyle(
-                              fontSize: 24, fontWeight: FontWeight.w300),
+                          "Sign up",
+                          style: TextStyle(fontSize: 24, color: Colors.white),
                         )),
                   ),
                 ],
               ),
-            )),
+            ),
+          )
+        ],
       ),
     );
   }
