8b0a7bd6d78b4f32cba630611e824b5c6730c752
aryanmaheshwari1420 <98485902+aryanmaheshwari1420@users.noreply.github.com>
2022-07-13 17:16:54 +0530

    making cards

--- whole_shop/lib/Home_page.dart
+++ whole_shop/lib/Home_page.dart
@@ -33,49 +33,148 @@ class _HomePageState extends State<HomePage> {
                       borderRadius: BorderRadius.circular(25.0),
                       child: TextFormField(
                         decoration: InputDecoration(
-                            prefixIcon: Icon(Icons.search, color: Colors.black),
-                            contentPadding: EdgeInsets.only(left: 15.0,top: 15.0),
-                            hintText: "Search for recipes",
-                            hintStyle: TextStyle(color: Colors.grey
-                            ),
+                          border: InputBorder.none,
+                          prefixIcon: Icon(Icons.search, color: Colors.black),
+                          contentPadding:
+                              EdgeInsets.only(left: 15.0, top: 15.0),
+                          hintText: "Search for recipes",
+                          hintStyle: TextStyle(color: Colors.grey),
+                        ),
                       ),
                     ),
-                  ),
                   ),
                   SizedBox(
                     height: 15.0,
                   ),
-                  Padding(padding: EdgeInsets.only(left: 15.0),
-                  child:Container(
-                    padding: EdgeInsets.only(left: 10.0),
-                    decoration: BoxDecoration(
-                      border: Border(left: BorderSide(color: Colors.orange,
-                      style: BorderStyle.solid,
-                      width: 3.0)
-                    ),
-                  ) ,
-                  child: Row(
-                    children: [
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                           Text('POPULAR RECIPES',
-                                  style: TextStyle(
-                                      fontSize: 20.0,
-                                      fontFamily: 'Timesroman',
-                                      fontWeight: FontWeight.bold)),
-                              Text('THIS WEEK',
-                                  style: TextStyle(
-                                      fontSize: 20.0,
-                                      fontFamily: 'Timesroman',
-                                      fontWeight: FontWeight.bold)),
-
-                        ],
-                      )
-                    ],
-
+                  Padding(
+                      padding: EdgeInsets.only(left: 15.0),
+                      child: Container(
+                        padding: EdgeInsets.only(left: 10.0),
+                        decoration: BoxDecoration(
+                          border: Border(
+                              left: BorderSide(
+                                  color: Colors.orange,
+                                  style: BorderStyle.solid,
+                                  width: 3.0)),
+                        ),
+                        child: Row(
+                          children: [
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text('POPULAR RECIPES',
+                                    style: TextStyle(
+                                        fontSize: 20.0,
+                                        fontFamily: 'Timesroman',
+                                        fontWeight: FontWeight.bold)),
+                                Text('THIS WEEK',
+                                    style: TextStyle(
+                                        fontSize: 20.0,
+                                        fontFamily: 'Timesroman',
+                                        fontWeight: FontWeight.bold)),
+                              ],
+                            )
+                          ],
+                        ),
+                      )),
+                  Padding(
+                    padding: EdgeInsets.only(bottom: 15.0),
                   ),
-                  ))
+                  Container(
+                    padding: EdgeInsets.only(top: 15.0, left: 15.0),
+                    height: 125.0,
+                    child: ListView(
+                      shrinkWrap: true,
+                      scrollDirection: Axis.horizontal,
+                      children: [
+                        _foodCard(),
+                        SizedBox(
+                          width: 10.0,
+                        ),
+                        _foodCard(),
+                        SizedBox(
+                          width: 10.0,
+                        ),
+                        _foodCard(),
+                        SizedBox(
+                          width: 10.0,
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              )
+            ],
+          ),
+          SizedBox(height: 15.0,),
+          Container(
+            alignment: Alignment.centerLeft,
+            padding: EdgeInsets.only(left: 15.0),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _foodCard() {
+    return Container(
+      height: 125.0,
+      width: 250.0,
+      decoration: BoxDecoration(
+          borderRadius: BorderRadiusDirectional.circular(12.0),
+          color: Colors.white),
+      child: Row(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(12.0),
+                image:
+                    DecorationImage(image: AssetImage("assets/balanced.jpg"))),
+            height: 125.0,
+            width: 100.0,
+          ),
+          SizedBox(
+            width: 20.0,
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Grilled Chicken',
+                style: TextStyle(fontFamily: 'Quicksand'),
+              ),
+              Text(
+                'with fruit Salad',
+                style: TextStyle(fontFamily: 'Quicksand'),
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              Container(
+                height: 2.0,
+                width: 75.0,
+                color: Colors.orange,
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                      height: 25.0,
+                      width: 25.0,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(12.5),
+                          image: DecorationImage(
+                              image: AssetImage('assets/chris.jpg')))),
+                  SizedBox(
+                    width: 10.0,
+                  ),
+                  Text(
+                    'James Oliver',
+                    style: TextStyle(fontFamily: 'Quicksand'),
+                  )
                 ],
               )
             ],
